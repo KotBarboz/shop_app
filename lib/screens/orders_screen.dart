7e36c73f22dd4e6efeaf9f4ee88c31418b5669e5
Konstantin <kotplansh@gmail.com>
2022-12-11 14:18:52 +0200

    add FutureBuilder for fetching orders

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -13,35 +13,43 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+  late Future _ordersFuture;
+
+  Future _obtainOrders() {
+    return Provider.of<Orders>(context, listen: false).fetchAnsSetOrders();
+  }
 
   @override
   void initState() {
+    _ordersFuture = _obtainOrders();
     super.initState();
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAnsSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your orders'),
       ),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-            ),
+      body: FutureBuilder(
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (snapshot.error != null) {
+              return const Center(child: Text('An error occurred!'));
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
+        future: _ordersFuture,
+      ),
       drawer: AppDrawer(),
     );
   }
